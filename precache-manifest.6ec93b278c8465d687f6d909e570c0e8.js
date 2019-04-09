1a81d95b385f760249c4c63cee7d94e266cc75f5
ThayalanGR <grthayalan18@gmail.com>
2019-04-10 00:03:56 +0530

    Updates

---
+++ precache-manifest.6ec93b278c8465d687f6d909e570c0e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e3e5a35f5661037e622",
+    "url": "/portfolio/static/css/main.f9db1389.chunk.css"
+  },
+  {
+    "revision": "0e3e5a35f5661037e622",
+    "url": "/portfolio/static/js/main.0e4685c1.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/portfolio/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a8303f995a616cfe4a1444a86b22fc80",
+    "url": "/portfolio/index.html"
+  }
+];
